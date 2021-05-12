07a971ff68506062764469a4ff6b773687c4a940
svetanguyen <matematicaforeber@gmail.com>
2021-05-13 00:37:50 +0700

    basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,130 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <div class="search-box">
+        <input type="text" placeholder="Search..." class="search-bar" />
+      </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">
+            Hanoi, Vietnam
+          </div>
+          <div class="date">
+            Wednesday 13 May 2021
+          </div>
+          <div class="weather-box">
+            <div class="temp">30°c</div>
+            <div class="weather">Cloudy</div>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      api_key: 'b9a95531348e9e64d3d477eeae75f958'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'montserrat', sans-serif;
+  }
+
+  #app {
+    background-image: url('./assets/cold-bg.jpeg');
+    background-size: cover;
+    background-position: bottom;
+    transition: .4s;
+  }
+
+  main {
+    min-height: 100vh;
+    padding: 25px;
+    background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0, 0, 0, .75));
+  }
+
+  .search-box {
+    width: 100%;
+    margin-bottom: 30px;
+  }
+
+  .search-box .search-bar {
+    display: block;
+    width: 100%;
+    padding: 15px;
+    color: #313131;
+    font-size: 20px;
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+    box-shadow: 0 0 8px rgba(0, 0, 0, .25);
+    background-color: rgba(255, 255, 255, .15);
+    border-radius: 0 16px 0 16px;
+    transition: .4s;
+  }
+
+  .search-box .search-bar:focus {
+    box-shadow: 0 0 16px rgba(0, 0, 0, .25);
+    background-color: rgba(255, 255, 255, .75);
+    border-radius: 16px 0 16px 0;
+  }
+
+  .location-box .location {
+    color: #fff;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: center;
+    text-shadow: 1px 3px rgba(0, 0, 0, .25);
+  }
+
+  .location-box .date {
+    color: #fff;
+    font-size: 20px;
+    font-weight: 300;
+    font-style: italic;
+    text-align: center;
+  }
+
+  .weather-box {
+    text-align: center;
+  }
+
+  .weather-box .temp {
+    display: inline-block;
+    padding: 10px 25px;
+    text-align: center;
+    color: #fff;
+    font-size: 102px;
+    font-weight: 900;
+    text-shadow: 3px 6px rgba(0, 0, 0, .25);
+    background-color: rgba(255, 255, 255, .25);
+    border-radius: 16px;
+    margin: 30px 0;
+    box-shadow: 3px 6px rgba(0, 0, 0, .25);
+  }
+
+  .weather-box .weather {
+    color: #fff;
+    font-size: 48px;
+    font-weight: 700;
+    font-style: italic;
+    text-shadow: 3px 6px rgba(0, 0, 0, .25);
+  }
 </style>
