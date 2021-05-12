8996b134a11b56ac914d5c97e3f88db4dfe5b4ed
svetanguyen <matematicaforeber@gmail.com>
2021-05-13 01:04:11 +0700

    refactor code

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
       <div class="search-box">
-        <input type="text" placeholder="Search..." class="search-bar" v-model="query" @keypress="fetchWeather"/>
+        <input type="text" placeholder="Search..." class="search-bar" v-model="query" @keyup.enter="fetchWeather"/>
       </div>
 
       <div class="weather-wrap" v-if="typeof weather.main !== 'undefined'">
@@ -37,13 +37,11 @@ export default {
   },
   methods: {
     fetchWeather (e) {
-      if (e.key === 'Enter') {
-        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
-          .then(res => {
-            return res.json()
-          })
-          .then(this.setResults)
-      }
+      fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        .then(res => {
+          return res.json()
+        })
+        .then(this.setResults)
     },
     setResults (results) {
       this.weather = results
